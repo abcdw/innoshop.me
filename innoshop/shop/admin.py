051b8ad9ce0e801e33a019863ed9a06be89b1414
Иван <ivan.plyusnin@gmail.com>
2015-08-23 15:54:15 +0300

    Add products to admin, add new fields to category

--- innoshop/shop/admin.py
+++ innoshop/shop/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Category
+from .models import Product
+
 
 @admin.register(Category)
 class Category(admin.ModelAdmin):
     list_display = ('name', 'parent')
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    def get_categories(self, obj):
+        return ", ".join([p.name for p in obj.categories.all()])
+
+    list_display = ('name', 'price', 'is_stock_empty', 'source_link', 'get_categories')
