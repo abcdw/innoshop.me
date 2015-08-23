75f282246cf657f290cdceafcb7a43e134d68ec0
Иван <ivan.plyusnin@gmail.com>
2015-08-23 14:40:25 +0300

    Add shop app and create models

--- innoshop/innoshop/settings.py
+++ innoshop/innoshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'orderform',
+    'shop',
 )
 
 MIDDLEWARE_CLASSES = (
--- innoshop/innoshop/urls.py
+++ innoshop/innoshop/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^order/', include('orderform.urls')),
+    url(r'^', include('shop.urls')),
 ]
