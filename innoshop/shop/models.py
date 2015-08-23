970c066eb38213b6e9540a152eb00aea903523db
Иван <ivan.plyusnin@gmail.com>
2015-08-23 15:34:08 +0300

    Add Category model to admin app and add shop/index view with list of categories

--- innoshop/shop/models.py
+++ innoshop/shop/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('self', null=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    def __unicode__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
