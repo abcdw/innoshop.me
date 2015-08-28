7f18892d0254cf8b203d3d4db87dfb198136ae74
abcdw <andrewtropin@gmail.com>
2015-08-28 10:50:09 +0300

    Remove orderform app

--- innoshop/innoshop/urls.py
+++ innoshop/innoshop/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^order/', include('orderform.urls')),
     url(r'^', include('shop.urls')),
 ]
