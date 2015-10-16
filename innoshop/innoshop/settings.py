46f0eea95d8ae9e0c021514693955f70d6651bc4
Ivan_Sharavuev <shpiwan@gmail.com>
2015-10-16 19:57:38 +0300

    Add IMAGE_ROOT

--- innoshop/innoshop/settings.py
+++ innoshop/innoshop/settings.py
@@ -121,14 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 FIXTURE_DIRS = (
-   os.path.join(BASE_DIR,'fixtures/'),
+    os.path.join(BASE_DIR, 'fixtures/'),
 
-   )
+    )
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+IMAGE_ROOT = os.path.join(STATIC_ROOT, 'img')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
