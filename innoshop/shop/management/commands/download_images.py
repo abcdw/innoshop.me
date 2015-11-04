8ac394cd705fc47b90ddad8c89a0286a73612a46
abcdw <andrewtropin@gmail.com>
2015-11-04 18:58:47 +0300

    Add script to download all images

--- innoshop/shop/management/commands/download_images.py
+++ innoshop/shop/management/commands/download_images.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from subprocess import call
+from shop.models import Product
+from urllib import urlretrieve
+import re
 
 
 class Command(BaseCommand):
@@ -7,9 +10,20 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('path', type=str)
-        parser.add_argument('url', type=str)
+        # parser.add_argument('url', type=str)
 
     def handle(self, *args, **options):
-        call(
-            "wget -O {0} {1}".format(options['path'], options['url']),
-            shell=True)
+        path = options['path']
+
+        product_count = Product.objects.all().count()
+        for p in Product.objects.all()[:10]:
+            file_name = re.sub(r'.*/', '', p.img_url)
+            file_path = path + '/' + file_name
+            try:
+                if p.id % 100 == 0:
+                    print p.id, '/', product_count
+                urlretrieve(p.img_url, file_path)
+            except Exception:
+                print p.id
+                break
+
