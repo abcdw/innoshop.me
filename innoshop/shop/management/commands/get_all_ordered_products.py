ab46ac7aa4d1d73dc61a0e74621b3ba6b1ab93a5
abcdw <andrewtropin@gmail.com>
2015-11-10 16:54:46 +0300

    Add get_all_ordered_products command

---
+++ innoshop/shop/management/commands/get_all_ordered_products.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from shop.models import Product, Order
+from collections import defaultdict
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        # parser.add_argument('poll_id', nargs='+', type=int)
+        pass
+
+    def handle(self, *args, **options):
+        # print Product.objects.get_sallable().count()
+        orders = Order.objects.all()
+        products_count = {}
+
+        product_count = defaultdict(int)
+        for order in orders:
+            for product_item in order.productitem_set.all():
+                product_count[product_item.SKU] += product_item.count
+
+        for k, v in product_count.iteritems():
+            print Product.objects.get(SKU=k).name, v
+
