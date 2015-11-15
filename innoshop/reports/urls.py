8b123925c6761f863994ee9b09b9fee68a20a488
Nikita Kraev <nightphoenix11@gmail.com>
2015-11-15 21:51:54 +0300

    Fixing url pattern (if i understood correctly, what is it)

--- innoshop/reports/urls.py
+++ innoshop/reports/urls.py
@@ -6,5 +6,5 @@ __author__ = 'kittn'
 urlpatterns = [
     url(r'^$', views.reports_page_view, name='reports_page'),
     url(r'^expeditor$', views.expeditor_view, name='reports_expeditor'),
-    url(r'^user', views.reports_user_view, name='reports_user'),
+    url(r'^user$', views.reports_user_view, name='reports_user'),
 ]
