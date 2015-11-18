14e18dcf52645eda9c1f514a8cc1c4267c48a762
Ivan_Sharavuev <shpiwan@gmail.com>
2015-11-18 12:23:36 +0300

    Fix style. Add flag --stats for statistics. 	modified:   from_xls_updater.py

--- innoshop/shop/management/commands/from_xls_updater.py
+++ innoshop/shop/management/commands/from_xls_updater.py
@@ -3,36 +3,45 @@ from django.core.management.base import BaseCommand, CommandError
 from shop.models import Product
 import math
 
+
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Update db from xls file.'
 
     def add_arguments(self, parser):
+        parser.add_argument('--stats',
+                            dest='stats',
+                            action='store_true',
+                            default=False)
         parser.add_argument('xls_path', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        
         source_book = xlrd.open_workbook(options['xls_path'][0])
         read_sheet = source_book.sheet_by_index(0)
         row = 4
-        row_tuple = read_sheet.row_values(3,start_colx=4,end_colx=10)
+        row_tuple = read_sheet.row_values(3, start_colx=4, end_colx=10)
         no_in_base = 0
-        errors = 0
-        while(any(row_tuple)):
+        while (any(row_tuple)):
             SKU = str(int(row_tuple[0]))
             price = math.ceil(row_tuple[-2])
             is_stock_empty = row_tuple[-1]
-           # Product.objects.filter(SKU=SKU).update(price=price,is_stock_empty=is_stock_empty)
-            p = Product.objects.filter(SKU=SKU)
-            if any(p):
-                p = p[0]
-                self.stdout.write("{0} {1} {2} {3}".format(SKU,p.actual_price,p.price,price))
+            if not options['stats']:
+                Product.objects.filter(
+                    SKU=SKU).update(price=price,
+                                    is_stock_empty=is_stock_empty)
             else:
-                no_in_base = no_in_base +1
+                p = Product.objects.filter(SKU=SKU)
+                if any(p):
+                    p = p[0]
+                    self.stdout.write("{0} {1} {2} {3}".format(
+                        SKU, p.actual_price, p.price, price))
+                else:
+                    no_in_base = no_in_base + 1
             try:
-                row_tuple = read_sheet.row_values(row,start_colx=4,end_colx=10)
+                row_tuple = read_sheet.row_values(row,
+                                                  start_colx=4,
+                                                  end_colx=10)
             except Exception as e:
                 break
-            row = row +1
-        self.stdout.write("No in db: {0}\nErrors: {1}\n".format(no_in_base,errors))
-
-
+            row = row + 1
+        if options['stats']:
+            self.stdout.write("No in db: {0}\n".format(no_in_base))
